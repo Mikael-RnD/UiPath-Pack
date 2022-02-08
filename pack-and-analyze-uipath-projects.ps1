6a532463f121ae2ea976401578aa7fd5d1093a4e
ML916 <ML916@users.noreply.github.com>
2022-02-08 10:36:19 +0100

    Update pack-and-analyze-uipath-projects.ps1

--- pack-and-analyze-uipath-projects.ps1
+++ pack-and-analyze-uipath-projects.ps1
@@ -1,4 +1,5 @@
 foreach($project in Get-ChildItem -Path $args[0] -Recurse -Filter project.json -File)
   {
 	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation
+	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package pack "$project" -o "$args[0]" --autoVersion -l en-US
   }
