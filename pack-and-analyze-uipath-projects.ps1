1aebdd213a98513a0d996ebb17625c8cab1d846d
ML916 <ML916@users.noreply.github.com>
2022-02-08 11:34:57 +0100

    Delete pack-and-analyze-uipath-projects.ps1

--- pack-and-analyze-uipath-projects.ps1
+++
@@ -1,5 +0,0 @@
-foreach($project in Get-ChildItem -Path $args[0] -Recurse -Filter project.json -File)
-  {
-	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation
-	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package pack "$project" -o "$args\output" --autoVersion -l en-US
-  }
