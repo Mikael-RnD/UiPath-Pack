c38e62c279d7d86f412a6cc38155ee55162b8119
ML916 <ML916@users.noreply.github.com>
2022-02-08 09:12:49 +0100

    Update pack-and-analyze-uipath-projects.ps1

--- pack-and-analyze-uipath-projects.ps1
+++ pack-and-analyze-uipath-projects.ps1
@@ -1,4 +1,4 @@
-foreach($package in Get-ChildItem -Path ${{ github.workspace }} -Recurse -Filter project.json -File)
+foreach($project in Get-ChildItem -Path ${{ github.workspace }} -Recurse -Filter project.json -File)
   {
-	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$package" --analyzerTraceLevel "Warning" --stopOnRuleViolation
+	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation
   }
