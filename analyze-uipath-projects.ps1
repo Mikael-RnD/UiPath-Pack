b20b0a447d257a752d17b0f59251994372f414e7
ML916 <ML916@users.noreply.github.com>
2022-02-10 20:32:31 +0100

    Update analyze-uipath-projects.ps1

--- analyze-uipath-projects.ps1
+++ analyze-uipath-projects.ps1
@@ -1,4 +1,3 @@
-#args[0]
 foreach($project in Get-ChildItem -Path $args[0] -Recurse -Filter project.json -File)
   {
 #	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation
