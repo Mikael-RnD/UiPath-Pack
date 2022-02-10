372e2bacdf09cf56b4082e269bde8d16e98ec0ae
Mikael Andersson <ml916smail@gmail.com>
2022-02-10 13:18:01 +0100

    Update pack-uipath-projects.ps1

--- pack-uipath-projects.ps1
+++ pack-uipath-projects.ps1
@@ -1,4 +1,5 @@
 foreach($project in Get-ChildItem -Path $args[0] -Recurse -Filter project.json -File)
   {
+    # Add authentication here
  	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package pack "$project" -o "$args\output" --autoVersion -l en-US
   }
