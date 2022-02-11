6b06604b11b55d91f044dcc18050f9ae9cc4380b
ML916 <ML916@users.noreply.github.com>
2022-02-11 21:28:42 +0100

    Update analyze-uipath-projects.ps1

--- analyze-uipath-projects.ps1
+++ analyze-uipath-projects.ps1
@@ -1,5 +1,5 @@
-foreach($project in Get-ChildItem -Path $workspace -Recurse -Filter project.json -File)
+foreach($project in Get-ChildItem -Path $args[0] -Recurse -Filter project.json -File)
   {
 #	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation
-	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation --orchestratorUrl "$url" --orchestratorTenant $tenant --orchestratorUsername *** --orchestratorPassword *** --orchestratorFolder "folder_name"
+	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package analyze "$project" --analyzerTraceLevel "Warning" --stopOnRuleViolation --orchestratorUrl "$(args[1])" --orchestratorTenant $(args[2]) --orchestratorUsername *** --orchestratorPassword *** --orchestratorFolder "folder_name"
   }
