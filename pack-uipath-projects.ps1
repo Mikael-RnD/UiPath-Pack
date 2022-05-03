f590137a92365cf2625265233cfe435cd3a42874
Mikael Andersson <ml916smail@gmail.com>
2022-05-03 19:06:24 +0200

    Delete pack-uipath-projects.ps1

--- pack-uipath-projects.ps1
+++
@@ -1,5 +0,0 @@
-foreach($project in Get-ChildItem -Path $args[0] -Recurse -Filter project.json -File)
-  {
-    # Add authentication here
- 	& "C:\\uipathcli\\lib\\net461\\uipcli.exe" package pack "$($project.FullName)" -o "$args\output" --autoVersion -l en-US
-  }
